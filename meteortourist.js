8996e8d101e21b4cac1e2ae55e62cea99939921d
uniquey <uniquey_1987@163.com>
2014-10-20 14:10:14 +0800

    selected item change colour

--- meteortourist.js
+++ meteortourist.js
@@ -5,19 +5,18 @@ if(Meteor.isClient){
     return PlayersList.find();
   };
   Template.leaderboard.events({
-    'focus input' :  function(){
-      console.log("You focus to the input")},
-    'blur input' :  function(){
-      console.log("You leave off the input")},
-    'change #addPlayerName' : function(){
-      console.log("You are inputing something")},
-    'mousedown' : function(){
-      console.log("Your mouse key is down")},
-    'keyup' : function(){
-      console.log("You are keying down")},
-    'submit' : function(){
-      console.log("Submit the input")},
-    'dblclick li.player' : function(){
-      Session.set('selectedPlayer',"Session value test")}, 
+    'click li.player' : function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer',playerId);
+      var selectedPlayer = Session.get('selectedPlayer'); 
+      console.log(selectedPlayer);
+    }
   });
-};
+  Template.leaderboard.selectedClass = function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    var playerId = this._id;
+    if(selectedPlayer === playerId){
+      return 'selected';
+    }
+  }
+}
