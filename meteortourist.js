7555acdf83285f0e1f432eb36a9677003416fc24
uniquey <uniquey_1987@163.com>
2014-10-21 16:13:04 +0800

    increase the score and sort in descending order of the list

--- meteortourist.js
+++ meteortourist.js
@@ -2,7 +2,10 @@ PlayersList = new Meteor.Collection('players')
 
 if(Meteor.isClient){
   Template.leaderboard.player = function(){
-    return PlayersList.find();
+    return PlayersList.find({},{sort:{score: -1}});
+    //'-1'here means to sort in descending order-
+    //meaning from the highest number to the lowest
+    //or from Z to A 
   };
   Template.leaderboard.events({
     'click li.player' : function(){
@@ -15,7 +18,9 @@ if(Meteor.isClient){
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(
         {_id: selectedPlayer},
-        {$set: {score:1}}
+        {$inc: {score:1}}
+        //pay attention that the '$set'or'$inc'must be here ,
+        //or the score one will instead the _id:selectedPlayer 
       );
     }
   });
